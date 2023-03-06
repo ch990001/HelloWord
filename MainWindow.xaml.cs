d8370db7670436ea42bd32ae7e84336b5404dbc0
ch990001 <ch990001@gmail.com>
2023-03-06 18:31:52 +0800

    進行介面基本設計
    增加一個按鍵和文字框

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace HelloWord
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
